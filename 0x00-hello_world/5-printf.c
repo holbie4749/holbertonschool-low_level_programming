7a81b6b16675df9de66aa4c35f5fa17cfcbaa8e9
holbie4749 <4749@holbertonschool.com>
2022-02-10 22:25:11 -0500

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0
+ * main - Entry point
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
